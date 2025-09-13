8152207d661b5b405238e9a37eecaaef5466c3b4
Florent Legname <florent.legname@gmail.com>
2025-09-13 20:51:55 +0200

    Changed: neotest-goland setup

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,15 +1,22 @@
 return {
-  { "nvim-neotest/neotest-plenary" },
   {
     "nvim-neotest/neotest",
-    opts = {
-      adapters = {
-        "neotest-plenary",
-        ["neotest-golang"] = {
-          go_test_args = { "-v", "-race" },
-          dap_go_enabled = true,
-        },
-      },
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "fredrikaverpil/neotest-golang", version = "*" }, -- Installation
     },
+    config = function()
+      local neotest_golang_opts = {
+        log_level = vim.log.levels.DEBUG,
+      } -- Specify custom configuration
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang")(neotest_golang_opts), -- Registration
+        },
+      })
+    end,
   },
 }
